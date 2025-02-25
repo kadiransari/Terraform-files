843f9fa5daf7568db3b59d739b8896d005396be4
Kadir Ansari <125560923+kadiransari@users.noreply.github.com>
2025-02-25 15:10:39 +0530

    Create variables.tf
    add variablefile

---
+++ variables/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+   default  = "us-west-2"
+}
+
+variable "accesskey" {
+   default  = "AKIAXTddsa.PGTL43LZJQ6V"
+}
+
+variable "secretkey" {
+   default  = "jjFSadsg.GfhIggxdjuJy8Hinwknys+h4LHE9qyIXvl"
+}
+
+variable "instancetype" {
+  default = "t2.medium"
+}
+
+variable "ami" {
+   default = "ami-00c257e12d6828491"
+}
